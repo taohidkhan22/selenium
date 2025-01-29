0f38f21a050dabafdc640c1d5cf29490ecde35d8
taohidkhan22 <taohid130@gmail.com>
2025-01-29 16:10:07 +0600

    Test_Facebook_SignUp_Day.py using X-Path

--- Test_Facebook_SignUp_Day.py
+++ Test_Facebook_SignUp_Day.py
@@ -4,11 +4,12 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
 driver.get("https://www.facebook.com/signup")
-elem= driver.find_element(By.ID,"day")
+# elem= driver.find_element(By.ID,"day")
+elem =driver.find_element(By.XPATH, "//select[@id='day']")
 alloption= elem.find_elements(By.TAG_NAME,"option")
 for option in alloption:
     print("value is: ",option.get_attribute("value"))
     option.click()
 
 
-## Test_Facebook_SignUp_Day 
+## Test_Facebook_SignUp_Day  
