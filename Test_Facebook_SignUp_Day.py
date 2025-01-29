fd2b474cca96906baf4d67ad80764008782c19b9
taohidkhan22 <taohid130@gmail.com>
2025-01-29 16:06:21 +0600

    Test_Facebook_SignUp_Day.py

--- Test_Facebook_SignUp_Day.py
+++ Test_Facebook_SignUp_Day.py
@@ -3,3 +3,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
+driver.get("https://www.facebook.com/signup")
+elem= driver.find_element(By.ID,"day")
+alloption= elem.find_elements(By.TAG_NAME,"option")
+for option in alloption:
+    print("value is: ",option.get_attribute("value"))
+    option.click()
+
+
+## Test_Facebook_SignUp_Day 
