43ab6707640eb557539b21d976f327d77a6bed0d
taohidkhan22 <taohid130@gmail.com>
2025-01-30 13:22:50 +0600

    Month field checking using select framework

--- FacebookSignUpForm/test_month.py
+++ FacebookSignUpForm/test_month.py
@@ -2,13 +2,18 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 
 driver= webdriver.Firefox()
 driver.get("https://www.facebook.com/signup")
 
-elem =driver.find_element(By.XPATH, "//select[@name='birthday_month']" )
+# elem =driver.find_element(By.XPATH, "//select[@name='birthday_month']" )
 
-alloption= elem.find_elements(By.TAG_NAME, "option")
-for option in alloption:
-    print("The value is:" , option.get_attribute("value"))
-    option.click()
+# alloption= elem.find_elements(By.TAG_NAME, "option")
+# for option in alloption:
+#     print("The value is:" , option.get_attribute("value"))
+#     option.click()
+
+
+select = Select(driver.find_element(By.NAME, "birthday_month"))
+select.select_by_value("4")
