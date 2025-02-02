65b51b73bf3c9ea634a4fe7d68961e38569b91ef
taohidkhan22 <taohid130@gmail.com>
2025-02-02 18:04:36 +0600

    click and hold file included using two classes

--- Drag and Drop/click_hold.py
+++ Drag and Drop/click_hold.py
@@ -1,9 +1,40 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 import time
 from time import sleep
+import unittest
 
-driver = webdriver.Firefox()
-driver.get("https://jqueryui.com/droppable/")
-driver.switch_to.frame(driver.find_element(By.TAG_NAME,"iframe"))
+class DragandDrop(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+    def test_draganddropcheck(self):
+        driver = self.driver
+        driver.get("https://jqueryui.com/droppable/")
+        driver.switch_to.frame(driver.find_element(By.TAG_NAME,"iframe"))
+        source= driver.find_element(By.ID, "draggable")
+        target= driver.find_element(By.ID, "droppable")
+        action_chains = ActionChains(driver)
+        time.sleep(5)
+        action_chains.drag_and_drop(source, target).perform()
+
+
+class moveToRelease(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+    def test_draganddropcheck(self):
+        driver = self.driver
+        driver.get("https://jqueryui.com/droppable/")
+        driver.switch_to.frame(driver.find_element(By.TAG_NAME,"iframe"))
+        source= driver.find_element(By.ID, "draggable")
+        target= driver.find_element(By.ID, "droppable")
+        action_chains = ActionChains(driver)
+        time.sleep(5)
+        action_chains.click_and_hold(source).move_to_element(target).release().perform()
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
