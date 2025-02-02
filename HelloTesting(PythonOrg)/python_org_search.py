ab053d0178583f0840ec87124a854fa6ac6b77e6
taohidkhan22 <taohid130@gmail.com>
2025-02-02 19:09:04 +0600

    Moving between windows and frames(mainly tab)

--- HelloTesting(PythonOrg)/python_org_search.py
+++ HelloTesting(PythonOrg)/python_org_search.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome()
-driver.get("http://www.python.org")
+import time
+from time import sleep
+driver = webdriver.Firefox()
+driver.get("http://www.python.org/about")
 assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
+# elem = driver.find_element(By.NAME, "q")
 # elem= driver.find_element(By.ID, "site-map")
 
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No Result Found" not in driver.page_source
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No Result Found" not in driver.page_source
+time.sleep(5)
+driver.forward()
+time.sleep(5)
+driver.back()
--- Moving window & Frame/windowSwitch.py
+++ Moving window & Frame/windowSwitch.py
@@ -2,28 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-
+import time
 driver=webdriver.Firefox()
 driver.get("https://www.facebook.com/signup")
-# select = Select(driver.find_element(By.XPATH,"//select[@id='birthday_wrapper']"))
-# all_selected_option = select.all_selected_options
-# options= select.options
+print("Opened Goggle")
 
-day_select= Select(driver.find_element(By.ID,"day"))
-month_select= Select(driver.find_element(By.ID,"month"))
-year_select =Select(driver.find_element(By.NAME, "birthday_year"))
+main_window= driver.current_window_handle
 
+# driver.execute_script("window.open('https://www.bing.com','_blank');")
 
-default_day = day_select.first_selected_option.text
-default_month = month_select.first_selected_option.text
-default_year= year_select.first_selected_option.text
-DOB= f"{default_day} {default_month} {default_year}"
+driver.execute_script("window.open('https://www.bing.com','_blank');")
 
-print(DOB)
+windows= driver.window_handles
+time.sleep(5)
 
-
-all_select = day_select.all_selected_options
-print(all_select[0].text)
-
-
-driver.switch_to.window('main')
+# for window in windows:
+#     if window!=main_window:
+#         driver.switch_to.window(window)
+#         break
+# print("Switched to Bing: ", driver.title)
+# time.sleep(5)
+driver.switch_to.window(main_window)
+print("Switched to goggle : ",driver.title)
