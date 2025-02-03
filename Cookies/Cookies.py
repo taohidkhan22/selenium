e050b194be3e659a7d760841ec0a4e87bfacc20e
taohidkhan22 <taohid130@gmail.com>
2025-02-03 10:59:40 +0600

    Cookies file are added

---
+++ Cookies/Cookies.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver= webdriver.Firefox()
+driver.get("https://www.google.co.uk/")
+cookie ={'name': 'foo', 'value': 'bar'}
+driver.add_cookie(cookie)
+i= driver.get_cookies()
+print("the cookies are: ",i)
